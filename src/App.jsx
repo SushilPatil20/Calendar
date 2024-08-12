43fe9d2538fffdb223cf1883d133530ded3308a5
PatilSushil20 <124390002+PatilSushil20@users.noreply.github.com>
2024-08-12 23:20:47 +0530

    core functionality is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import {
+  days,
+  getCurrentMonth,
+  getCurrentYear,
+  getCurrentDay,
+  months,
+} from "./utils/script";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dates, setDates] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth);
+  const [currentYear, setCurrentYear] = useState(getCurrentYear);
+
+  const increseMonth = () => {
+    const newMonth = currentMonth < 11 ? currentMonth + 1 : 0;
+    setCurrentMonth(newMonth);
+  };
+  const decreseMonth = () => {
+    const newMonth = currentMonth > 0 ? currentMonth - 1 : months.length - 1;
+    setCurrentMonth(newMonth);
+  };
+
+  useEffect(() => {
+    const newDates = [];
+    const year = currentYear;
+    const month = currentMonth;
+
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    for (let i = 0; i < firstDay.getDay(); i++) {
+      newDates.push(null);
+    }
+    setDates([...dates, ...newDates]);
+
+    for (let day = 1; day <= lastDay.getDate(); day++) {
+      newDates.push(day);
+    }
+    setDates([...newDates]);
+  }, [currentMonth]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="text-center">
+        <h1 className="text-6xl font-thin my-12">Calendar</h1>
+      </header>
+      <section className="px-8 md:w-1/2 md:mx-auto">
+        <section className="flex items-center justify-between h-10 py-8">
+          <span className="text-xl md:text-4xl">{currentYear}</span>
+          <div className="space-x-2 md:space-x-4">
+            <button
+              className="md:text-4xl text-gray-800"
+              onClick={decreseMonth}
+            >
+              <iconify-icon icon="bxs:left-arrow"></iconify-icon>
+            </button>
+            <span className="md:text-2xl">{months[currentMonth]}</span>
+            <button
+              className="md:text-4xl text-gray-800"
+              onClick={increseMonth}
+            >
+              <iconify-icon icon="bxs:right-arrow"></iconify-icon>
+            </button>
+          </div>
+        </section>
+        <section className="w-full grid grid-cols-7">
+          {days.map((day, index) => (
+            <section
+              key={index}
+              className={`h-14 flex text-sm items-center justify-center border bg-black text-white border-gray-500`}
+            >
+              {day}
+            </section>
+          ))}
+          {dates.map((date, index) => (
+            <section
+              key={index}
+              className={`${
+                getCurrentDay === date
+                  ? "bg-blue-700 text-white font-semibold"
+                  : ""
+              } h-14 flex text-xl items-center justify-center border`}
+            >
+              {date}
+            </section>
+          ))}
+        </section>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/script.js
@@ -0,0 +1,10 @@
+export const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const date = new Date();
+export const getCurrentMonth = date.getMonth()
+export const getCurrentYear = date.getFullYear();
+export const getCurrentDay = date.getDate();
+export const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
